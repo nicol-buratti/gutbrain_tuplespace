64a8e975cdb1156171dd11fd3a27539872832d3b
nicol-buratti <nickibrt8@gmail.com>
2025-01-14 14:59:26 +0100

    add yaml env

--- src/main/java/it/unicam/gutbrain/Main.java
+++ src/main/java/it/unicam/gutbrain/Main.java
@@ -5,23 +5,35 @@ import it.unicam.gutbrain.gut.ProteinStatus;
 import it.unicam.gutbrain.gut.ProteinType;
 import org.jspace.SequentialSpace;
 import org.jspace.Space;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] argv) throws InterruptedException {
+    public static void main(String[] argv) throws InterruptedException, IOException {
         Space space = new SequentialSpace();
         space.put("GUT", 0); // impermeability
         //space.put("BRAIN")
 
         Master master = new Master(space);
 
-        for (int i = 0; i < 5; i++) {
+        Yaml yaml = new Yaml();
+        InputStream inputStream = Files.newInputStream(new File("C:\\Users\\nicki\\IdeaProjects\\gutbrain\\env.yaml").toPath());
+        Map<String, Integer> env = yaml.load(inputStream);
+
+        for (int i = 0; i < env.get("aep_enzyme"); i++) {
             space.put("CREATE", "AEP");
         }
 
-        space.put("PROTEIN", ProteinType.ALPHA, ProteinStatus.NORMAL, 50);
-        space.put("PROTEIN", ProteinType.TAU, ProteinStatus.NORMAL, 50);
-        space.put("OLIGOMER", ProteinType.ALPHA, 0);
-        space.put("OLIGOMER", ProteinType.TAU, 0);
+
+        space.put("PROTEIN", ProteinType.ALPHA, ProteinStatus.NORMAL, env.get("alpha_proteins"));
+        space.put("PROTEIN", ProteinType.TAU, ProteinStatus.NORMAL, env.get("tau_proteins"));
+        space.put("OLIGOMER", ProteinType.ALPHA, env.get("alpha_oligomers"));
+        space.put("OLIGOMER", ProteinType.TAU, env.get("tau_oligomers"));
 
 
         master.run();
