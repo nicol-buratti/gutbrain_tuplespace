542b0bdb9ab5bf0b3da65a355b9ebd09069d32e6
nicol-buratti <nickibrt8@gmail.com>
2025-01-20 17:26:31 +0100

    implemented threshold in microglia

--- src/main/java/it/unicam/gutbrain/brain/MicrogliaAgent.java
+++ src/main/java/it/unicam/gutbrain/brain/MicrogliaAgent.java
@@ -6,6 +6,8 @@ import org.jspace.ActualField;
 import org.jspace.FormalField;
 import org.jspace.Space;
 
+import static it.unicam.gutbrain.Config.env;
+
 public class MicrogliaAgent implements Runnable {
 
     private final Space space;
@@ -19,8 +21,12 @@ public class MicrogliaAgent implements Runnable {
     @Override
     @SneakyThrows
     public void run() {
+        int threshold = env.get("permeability_threshold");
         while (true) {
-            // TODO add threshold
+            Object[] gut = space.query(new ActualField("GUT"), new FormalField(Integer.class));
+            if ((int) gut[1] < threshold)
+                continue;
+
             Object[] oligomers = space.get(new ActualField("OLIGOMER"), new FormalField(ProteinType.class), new FormalField(Integer.class));
             if ((int) oligomers[2] == 0) {
                 space.put(oligomers[0], oligomers[1], oligomers[2]);
