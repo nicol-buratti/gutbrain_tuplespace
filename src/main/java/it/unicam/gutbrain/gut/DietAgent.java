a13392f91034ba42e318db57243d68407a5034ca
nicol-buratti <nickibrt8@gmail.com>
2025-01-24 16:12:50 +0100

    refactoring diet

--- src/main/java/it/unicam/gutbrain/gut/DietAgent.java
+++ src/main/java/it/unicam/gutbrain/gut/DietAgent.java
@@ -11,55 +11,76 @@ import java.util.logging.Logger;
 public class DietAgent implements Runnable {
 
     private static final Logger logger = Logger.getLogger(DietAgent.class.getName());
+    private static final int MAX_SLEEP_TIME = 1300;
+    private static final int DIGESTIVE_TIME = 500;
+
     private final Space space;
+    private final Random random;
 
     public DietAgent(Space space) {
         this.space = space;
+        this.random = new Random();
     }
 
     @Override
     @SneakyThrows
     public void run() {
-        Random random = new Random();
         while (true) {
-            Thread.sleep(Math.abs(random.nextLong() % 100));
-            Object[] diet = space.get(new ActualField("DIET"), new FormalField(Integer.class),
-                    new FormalField(Integer.class), new FormalField(Integer.class));
+            Thread.sleep(getRandomSleepTime(MAX_SLEEP_TIME));
 
-            float goodBacteria = 1;
-            float badBacteria = 1;
+            Object[] diet = fetchDietFromSpace();
+            int sugar = adjustSugar((int) diet[1]);
+            int milk = adjustMilk((int) diet[2]);
+            int salt = adjustSalt((int) diet[3]);
 
-            int sugar = (int) diet[1];
-            int milk = (int) diet[2];
-            int salt = (int) diet[3];
+            updateBacteriaLevels(milk);
 
-            sugar -= Math.max(0, random.nextInt() % 15);
-            milk -= Math.max(0, random.nextInt() % 300);
-            salt -= Math.max(0, random.nextInt() % 2);
-
-
-            if (milk > 500) {
-                goodBacteria -= 0.5;
-                badBacteria += 0.5;
-            }
-
-            Object[] bacteria = space.get(new ActualField("BACTERIA"), new ActualField(BacteriaStatus.GOOD), new FormalField(Integer.class));
-            int updatedGoodBacteria = (int) ((int) bacteria[2] * goodBacteria);
-            space.put("BACTERIA", BacteriaStatus.GOOD, updatedGoodBacteria);
-            logger.info("BACTERIA GOOD: " + updatedGoodBacteria);
-
-            bacteria = space.get(new ActualField("BACTERIA"), new ActualField(BacteriaStatus.PATHOGENIC), new FormalField(Integer.class));
-            int updatedPathogenicBacteria = (int) ((int) bacteria[2] * badBacteria);
-            space.put("BACTERIA", BacteriaStatus.PATHOGENIC, updatedPathogenicBacteria);
-            logger.info("BACTERIA PATHOGENIC: " + updatedPathogenicBacteria);
-
-
-            Thread.sleep(Math.abs(random.nextLong() % 500)); // digestive time
-            int addSugar = Math.abs(random.nextInt() % 20);
-            int addMilk = Math.abs(random.nextInt() % 350);
-            int addSalt = Math.abs(random.nextInt() % 3);
-            space.put("DIET", sugar + addSugar, milk + addMilk, salt + addSalt);
-            logger.info("Mangiato " + addSugar + " " + addMilk + " " + addSalt);
+            Thread.sleep(getRandomSleepTime(DIGESTIVE_TIME)); // Simulate digestion
+            replenishDiet(sugar, milk, salt);
         }
+    }
+
+    private int getRandomSleepTime(int maxTime) {
+        return Math.abs(random.nextInt() % maxTime);
+    }
+
+    private Object[] fetchDietFromSpace() throws InterruptedException {
+        return space.get(new ActualField("DIET"), new FormalField(Integer.class), new FormalField(Integer.class), new FormalField(Integer.class));
+    }
+
+    private int adjustSugar(int sugar) {
+        return sugar - Math.max(0, random.nextInt() % 15);
+    }
+
+    private int adjustMilk(int milk) {
+        return milk - Math.max(0, random.nextInt() % 300);
+    }
+
+    private int adjustSalt(int salt) {
+        return salt - Math.max(0, random.nextInt() % 2);
+    }
+
+    private void updateBacteriaLevels(int milk) throws InterruptedException {
+        float goodBacteriaFactor = milk > 500 ? 0.5f : 1;
+        float badBacteriaFactor = milk > 500 ? 1.5f : 1;
+
+        updateBacteria(BacteriaStatus.GOOD, goodBacteriaFactor);
+        updateBacteria(BacteriaStatus.PATHOGENIC, badBacteriaFactor);
+    }
+
+    private void updateBacteria(BacteriaStatus status, float factor) throws InterruptedException {
+        Object[] bacteria = space.get(new ActualField("BACTERIA"), new ActualField(status), new FormalField(Integer.class));
+        int updatedBacteria = Math.round((int) bacteria[2] * factor);
+        space.put("BACTERIA", status, updatedBacteria);
+        logger.info("BACTERIA " + status + ": " + updatedBacteria);
+    }
+
+    private void replenishDiet(int sugar, int milk, int salt) throws InterruptedException {
+        int addSugar = Math.abs(random.nextInt() % 20);
+        int addMilk = Math.abs(random.nextInt() % 350);
+        int addSalt = Math.abs(random.nextInt() % 3);
+
+        space.put("DIET", sugar + addSugar, milk + addMilk, salt + addSalt);
+        logger.info("Mangiato " + addSugar + " " + addMilk + " " + addSalt);
     }
 }
