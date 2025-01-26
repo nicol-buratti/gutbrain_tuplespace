3d1aa71602a30fdf0be06dcda94e4fd771617f93
nicol-buratti <nickibrt8@gmail.com>
2025-01-26 11:25:39 +0100

    modified activity of cleaved protein

--- src/main/java/it/unicam/gutbrain/gut/CleavedProteinAgent.java
+++ src/main/java/it/unicam/gutbrain/gut/CleavedProteinAgent.java
@@ -24,38 +24,45 @@ public class CleavedProteinAgent implements Runnable {
     @SneakyThrows
     public void run() {
         Random random = new Random();
-        boolean shouldBeDestroyed = space.getp(new ActualField("DESTROY"), new ActualField(this.proteinType),
-                new ActualField(ProteinStatus.CLEAVED)) != null;
+        int size = random.nextInt(3) + 2; // range 2 - 5
 
-        while (!shouldBeDestroyed) {
-            int size = random.nextInt(3) + 2; // range 2 - 5
-            int i = 0;
-            while (i < size) {
-                shouldBeDestroyed = space.getp(new ActualField("DESTROY"), new ActualField(this.proteinType),
-                        new ActualField(ProteinStatus.CLEAVED)) != null;
-                if (shouldBeDestroyed)
-                    break;
+        // checks if it should be destroyed
+        Object[] destroy = space.getp(new ActualField("DESTROY"), new ActualField(proteinType), new ActualField(ProteinStatus.CLEAVED), new FormalField(Integer.class));
+        if (destroy == null)
+            space.put("DESTROY", proteinType, ProteinStatus.CLEAVED, 0);
+        else if ((int) destroy[3] > 0) {
+            space.put(destroy[0], destroy[1], destroy[2], (int) destroy[3] - 1);
+            return;
+        } else {
+            space.put(destroy[0], destroy[1], destroy[2], destroy[3]);
+        }
 
-                Object[] protein = space.get(new ActualField("PROTEIN"), new ActualField(proteinType),
-                        new ActualField(ProteinStatus.CLEAVED), new FormalField(Integer.class));
-                if ((int) protein[3] == 0) {
-                    space.put(protein[0], protein[1], protein[2], protein[3]);
-                    continue;
-                }
-                space.put(protein[0], protein[1], protein[2], (int) protein[3] - 1);
-                space.put("DESTROY", proteinType, ProteinStatus.CLEAVED);
 
-                i++;
-                Thread.sleep(800);
-            }
-            if (shouldBeDestroyed)
+        while (true) {
+            Object[] cleavedProteins = space.get(
+                    new ActualField("PROTEIN"),
+                    new ActualField(proteinType),
+                    new ActualField(ProteinStatus.CLEAVED),
+                    new FormalField(Integer.class));
+            // creates the oligomer
+            if ((int) cleavedProteins[3] >= size) {
+                space.put(cleavedProteins[0], cleavedProteins[1], cleavedProteins[2], (int) cleavedProteins[3] - size);
+                Object[] oligomers = space.get(new ActualField("OLIGOMER"), new ActualField(proteinType), new FormalField(Integer.class));
+                space.put(oligomers[0], oligomers[1], (int) oligomers[2] + 1);
+                logger.info("Oligomer created: " + Arrays.toString(oligomers));
+
+                Object[] destroyTuple = space.get(new ActualField("DESTROY"), new ActualField(proteinType), new ActualField(ProteinStatus.CLEAVED), new FormalField(Integer.class));
+                space.put(destroyTuple[0], destroyTuple[1], destroyTuple[2], (int) destroyTuple[3] + size);
                 break;
-
-            // get oligomers counter tuple and update it
-            Object[] tuple = space.get(new ActualField("OLIGOMER"), new ActualField(this.proteinType), new FormalField(Integer.class));
-            space.put(tuple[0], tuple[1], (int) tuple[2] + 1);
-
-            logger.info("Creato Oligomer" + Arrays.toString(tuple));
+            }
+            // protein failed the creation and check if it should be destroyed
+            space.put(cleavedProteins[0], cleavedProteins[1], cleavedProteins[2], cleavedProteins[3]);
+            Object[] destroyTuple = space.get(new ActualField("DESTROY"), new ActualField(proteinType), new ActualField(ProteinStatus.CLEAVED), new FormalField(Integer.class));
+            if ((int) destroyTuple[3] > 0) {
+                space.put(destroyTuple[0], destroyTuple[1], destroyTuple[2], (int) destroyTuple[3] - 1);
+                break;
+            }
+            space.put(destroyTuple[0], destroyTuple[1], destroyTuple[2], destroyTuple[3]);
         }
     }
 }
